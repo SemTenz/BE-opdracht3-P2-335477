910748629d05243278980f96c6fbb4e670b3feae
SemTenz <sem.jaeger@ziggo.nl>
2023-11-26 23:18:15 +0100

    6de commit

--- app/config/config.php
+++ app/config/config.php
@@ -3,5 +3,5 @@
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'be-opdracht1-j2-p1');
-define('URLROOT', 'http://www.be-opdracht3-j2-p1.com');
+define('DB_NAME', 'be-opdracht-03');
+define('URLROOT', 'http://be-opdracht3-j2-p2');
